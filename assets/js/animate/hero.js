45a56ed7279daf57c3deb6b71a0279e03e1c73bd
kingno3l <nuelluiz@gmail.com>
2024-10-05 01:08:16 +0100

    fixed linters

--- assets/js/animate/hero.js
+++ assets/js/animate/hero.js
@@ -1,12 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Show the hero section after 1 second
+  setTimeout(() => {
+    document.querySelector('.hero').classList.add('visible');
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Show the hero section after 1 second
-    setTimeout(function() {
-        document.querySelector('.hero').classList.add('visible');
-
-       // Show the image after 1 second
-setTimeout(function() {
-    document.querySelector('.hero img').classList.add('visible');
-}, 1000); // Delay of 1000ms (1 second) for the image
-    }, 1000); // Delay of 1000ms (1 second) for the hero section
+    // Show the image after 1 second
+    setTimeout(() => {
+      document.querySelector('.hero img').classList.add('visible');
+    }, 1000); // Delay of 1000ms (1 second) for the image
+  }, 1000); // Delay of 1000ms (1 second) for the hero section
 });
